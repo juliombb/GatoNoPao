adf48300b34eb55fbbcf39ee63d8b6a73a25f2c4
Akacoll <gabriel.akacoll@gmail.com>
2017-04-08 17:01:22 -0300

    implementação de hitbox e flip na sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 	static int vida = 3;
+	bool estaVirado = false;
 	float speed = 10.0f;
 	const float cadencia = 0.5f;
 	float intervalo = 0.0f;
@@ -12,6 +13,12 @@ public class Player : MonoBehaviour {
 	public GameObject tiro;
 	public GameObject novelo;
 	GameObject tiroAtual;
+
+	private SpriteRenderer mySpriteRenderer;
+
+	void Awake(){
+		mySpriteRenderer = GetComponent<SpriteRenderer>();
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -30,6 +37,30 @@ public class Player : MonoBehaviour {
 			//gameover
 		}
 		this.transform.position += new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0) * speed * Time.deltaTime;
+		if (mySpriteRenderer != null) {
+			// if the A key was pressed this frame
+			if ((Input.GetAxis ("Horizontal") < 0f) && (estaVirado == false)) {
+				mySpriteRenderer.flipX = true;
+				estaVirado = true;
+			} else if ((Input.GetAxis ("Horizontal") > 0f) && (estaVirado)){
+				mySpriteRenderer.flipX = false;
+				estaVirado = false;
+		}
+		}
+		float posX = transform.position.x;
+		float posY = transform.position.y;
+
+		if (posX <= -11.0f) {
+			transform.position = new Vector3 (-11.0f, posY, -2.0f);
+		}else if (posX >= 11.0f) {
+			transform.position = new Vector3 (11.0f, posY, -2.0f);
+		}
+
+		if (posY <= -4.25f) {
+			transform.position = new Vector3 (posX, -4.25f, -2.0f);
+		}else if (posY >= 4.0f) {
+			transform.position = new Vector3 (posX, 4.0f, -2.0f);
+		}
 	}
 	void OnTriggerEnter2D (Collider2D col){
 		if (col.gameObject.tag.Equals ("Obstacle")) {
