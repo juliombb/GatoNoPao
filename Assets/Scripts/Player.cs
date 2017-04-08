61f4f909c01a21d04957f61fc6c7e3cc3d453181
Akacoll <gabriel.akacoll@gmail.com>
2017-04-08 08:50:46 -0300

    implementação da vida

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 	int vida = 3;
@@ -8,16 +9,18 @@ public class Player : MonoBehaviour {
 	const float cadencia = 0.5f;
 	float intervalo = 0.0f;
 	public GameObject tiro;
+	public Text lblScore;
 	GameObject tiroAtual;
 
 	// Use this for initialization
 	void Start () {
+		lblScore.text = ""+ vida;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		intervalo += Time.deltaTime;
-		if (Input.GetButton("Fire1") && intervalo >= cadencia) {
+		if ((Input.GetButton("Fire1")||Input.GetButton("Jump"))  && intervalo >= cadencia) {
 			tiroAtual = Instantiate(tiro);
 			tiroAtual.transform.position = this.transform.position + new Vector3(1.0f, 0.0f, 0.0f);
 			intervalo = 0;
@@ -28,11 +31,16 @@ public class Player : MonoBehaviour {
 		}
 		this.transform.position += new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0) * speed * Time.deltaTime;
 	}
-
 	void OnTriggerEnter2D (Collider2D col){
-		if (col.gameObject.tag.Equals("Obstacle")) {
+		if (col.gameObject.tag.Equals ("Obstacle")) {
 			vida -= 1;
-			col.gameObject.transform.position = new Vector3(12.0f, Random.Range(-5.0f,5.0f),0.0f);
+			col.gameObject.transform.position = new Vector3 (12.0f, Random.Range (-5.0f, 5.0f), 0.0f);
+		}
+	void OnTriggerEnter (Collider col){ // levou um tiro
+		if (col.gameObject.name.Equals("projetil Gato(Clone)")||(col.gameObject.name.Equals("projetil Gato"))) {
+			vida -= 10;
+			lblScore.text =  "" + vida;
+			Destroy (col.gameObject);
 		}	
 	}
 }
